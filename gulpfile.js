fd2f4527b29c66cb4cef3e88acf00ecf89e952ff
Rijvy <rrijvy@gmail.com>
2018-07-12 23:00:04 +0600

    Get css workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,23 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'); 
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested') ;
+
 
 gulp.task('default', function(){
     console.log('Rijvy');
 });
 
 gulp.task('styles', function(){
-    return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+    return gulp.src('./app/assets/styles/styles.css') //source css
+    .pipe(postcss([cssvars, nested, autoprefixer])) //diffrent version css
+    .pipe(gulp.dest('./app/temp/styles')); //destination css
 });
 
-gulp.watch('watch',function(){
-    gulp.watch('./app/assets/styles/styles.css', function(){
+gulp.task('watch',function(){
+    watch('./app/assets/styles/**/*.css', function(){
         gulp.start('styles');
     });    
 });
