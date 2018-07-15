55e40109314dbb7a2d10475da731dc368f35c351
Rijvy <rrijvy@gmail.com>
2018-07-15 18:30:21 +0600

    gulpfile.js error handle

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import') ;
+var gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  cssvars = require("postcss-simple-vars"),
+  nested = require("postcss-nested"),
+  cssImport = require("postcss-import");
 
-
-gulp.task('default', function(){
-    console.log('Rijvy');
+gulp.task("default", function() {
+  console.log("Rijvy");
 });
 
-gulp.task('styles', function(){
-    return gulp.src('./app/assets/styles/styles.css') //source css
+gulp.task("styles", function() {
+  return gulp
+    .src("./app/assets/styles/styles.css") //source css
     .pipe(postcss([cssImport, cssvars, nested, autoprefixer])) //diffrent version css
-    .pipe(gulp.dest('./app/temp/styles')); //destination css
+    .on("error", function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit("end");
+    })
+    .pipe(gulp.dest("./app/temp/styles")); //destination css
 });
 
-gulp.task('watch',function(){
-    watch('./app/assets/styles/**/*.css', function(){
-        gulp.start('styles');
-    });    
+gulp.task("watch", function() {
+  watch("./app/assets/styles/**/*.css", function() {
+    gulp.start("styles");
+  });
 });
